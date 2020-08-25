b8d49c0903ff9e51564d5551bfcac3ffdd268096
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2020-08-25 07:35:37 -0400

    Make file now works.

--- makeFile.c
+++ makeFile.c
@@ -4,5 +4,24 @@
 int main() {
     printf("\nYou are in: \n");
     system("pwd");
+
+    FILE * fPtr;
+    char fileName[50];
+
+    printf("Enter file name: ");
+    scanf("%s", fileName);
+
+    fPtr = fopen(fileName, "w");
+
+    if(fPtr == NULL) {
+        printf("Unable to create file.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    fclose(fPtr);
+
+
+    printf("File created successfully. \n");
+
     exit(0);
 }
